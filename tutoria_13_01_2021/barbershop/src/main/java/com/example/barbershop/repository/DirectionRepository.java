fa87579124a3d10702f174ce933571f06a86555a
magnerronny <rjimenezampuero@gmail.com>
2022-09-24 00:55:38 -0500

    Revert "barbershop one-to-one"
    This reverts commit df555cb578c5322c4b9ab6ca2a462be3082bf0bd.

--- tutoria_13_01_2021/barbershop/src/main/java/com/example/barbershop/repository/DirectionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.barbershop.repository;
-
-import com.example.barbershop.entities.Direction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DirectionRepository extends JpaRepository<Direction, Long> {
-}
